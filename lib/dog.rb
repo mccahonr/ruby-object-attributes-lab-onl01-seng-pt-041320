ae942359bd9e374b3d7a58e0d9021d7778df935d
Ryan McCahon <mccahonr@yahoo.com>
2020-05-01 17:47:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,4 @@
 class Dog
- def name=(imya)
-   @dog_name = imya
- end
- def name
-   @dog_name
- end
  
- def breed=(race)
-   @dog_breed = race
- end
- def breed
-   @dog_breed
- end
+ def initialize()
 end
